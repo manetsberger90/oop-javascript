938996c3111aaa52dc344dc80ef1ed65547d57eb
Michael <manetsberger90@gmail.com>
2015-02-23 14:02:08 -0800

    For In Loop Lesson

--- js/object.js
+++ js/object.js
@@ -8,19 +8,6 @@ var person = {
     skills: ['JavaScript', 'CSS', 'HTML']
 };
 
-function print(message) {
-    var div = document.getElementById('output');
-    div.innerHTML = message;
+for (prop in person) {
+document.write.listStyle(prop, ': ', person[prop], ', ')   
 }
-
-var message = '<p> Hello. My name is ' + person.name + '</p>';
-    message += '<p> I am from the country ' + person.country + '</p>';
-    person.name = 'Rainbow Dash';
-    message +=  '<p>But I wish my name was ' + person.name + '</p>';
-    person.age += 1;
-    message += '<p> My age is now ' + person.age + '</p>';
-    message += '<p> I have ' + person.skills.length + ' skills: ';
-    message += person.skills.join(', ') + '</p>';
-
-
-print(message);
